39bdbbb509881f1b74ba21a0b44eeddbdc77ece0
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-29 08:39:24 +0900

    Add Framework project

---
+++ LLAEasyAlertManager/LLAEasyAlertManager/LLAEasyAlertManager.h
@@ -0,0 +1,19 @@
+//
+//  LLAEasyAlertManager.h
+//  LLAEasyAlertManager
+//
+//  Created by Daisuke T on 2019/01/28.
+//  Copyright © 2019 LLAEasyAlertManager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LLAEasyAlertManager.
+FOUNDATION_EXPORT double LLAEasyAlertManagerVersionNumber;
+
+//! Project version string for LLAEasyAlertManager.
+FOUNDATION_EXPORT const unsigned char LLAEasyAlertManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLAEasyAlertManager/PublicHeader.h>
+
+
---
+++ LLAEasyAlertManager/LLAEasyAlertManager/LLAEasyAlertManager.swift
@@ -0,0 +1,59 @@
+//
+//  LLAEasyAlertManager.swift
+//  LLAEasyAlertManager
+//
+//  Created by Daisuke T on 2019/01/28.
+//  Copyright © 2019 LLAEasyAlertManager. All rights reserved.
+//
+
+import Foundation
+
+
+
+public class LLAEasyAlertManager
+{
+	// MARK: Singleton
+	public static let sharedInstance = LLAEasyAlertManager()
+
+	
+	
+	// MARK: - Property
+	private static let actionTitleDefaultPreset = "OK"
+	private static let actionTitleCancelPreset = "Cancel"
+	public var actionTitleMap = [UIAlertAction.Style.default: actionTitleDefaultPreset,
+								 UIAlertAction.Style.cancel: actionTitleCancelPreset]
+	
+	
+	
+	// MARK: - Present
+	public func present(_ viewController: UIViewController,
+						message: String,
+						title: String = "",
+						handlerDefault: ((UIAlertAction) -> Void)? = nil,
+						handlerCancel: ((UIAlertAction) -> Void)? = nil) {
+
+		// Create alert.
+		let alert = UIAlertController(title: title,
+									  message: message,
+									  preferredStyle: .alert)
+
+		
+		// Create action.
+		let action = UIAlertAction(title: actionTitleMap[.default],
+								   style: .default,
+								   handler: handlerDefault)
+		alert.addAction(action)
+
+		if handlerCancel != nil {
+			let action = UIAlertAction(title: actionTitleMap[.cancel],
+									   style: .cancel,
+									   handler: handlerCancel)
+			alert.addAction(action)
+		}
+
+
+		// Present.
+		viewController.present(alert, animated: true, completion: nil)
+	}
+	
+}
